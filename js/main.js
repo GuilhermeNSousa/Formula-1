75ca6ee7c2d68677d0f1f3af42d41fed6ea4a8ac
GuilhermeNSousa <nascimentoguilherme66@gmail.com>
2024-06-14 12:24:34 -0300

    Adicionando cards

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
-function fazGet(url){
-    let request = new XMLHttpRequest()
-    request.open("GET", url, false)
-    request.send()
-    return request.responseText
+function insertStandings(){
+    fetch("http://ergast.com/api/f1/2024/driverStandings.json")
+        .then((response) => response.json())
+        .then((pilotos) => {
+            var standings = pilotos.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+            var container = document.querySelector("#pilots-container");
+
+            for (var i = 0; i < 3; i++){
+                console.log(standings[i]);
+
+                var card = document.createElement("div");
+                card.classList.add("card");
+
+                var elements = document.createElement("div");
+                elements.classList.add("cardElements");
+
+                var picture = document.createElement("img");
+                picture.classList.add("picture");                
+                picture.src = ".\\img\\Pilots\\" + standings[i].Driver.givenName + "_" + standings[i].Driver.familyName + ".png";
+                
+                var texts = document.createElement("div");
+                texts.classList.add("cardTexts")
+
+                var pilot = document.createElement("h3");
+                pilot.textContent = standings[i].Driver.givenName + " " + standings[i].Driver.familyName.toUpperCase();
+
+                var position = document.createElement("h3");
+                position.textContent = "Position: " + standings[i].position;
+
+                
+                texts.appendChild(pilot);
+                texts.appendChild(position);
+
+                elements.appendChild(picture);
+                elements.appendChild(texts);
+
+                card.appendChild(elements);
+
+                container.appendChild(card);
+            }
+    })
 }
 
-function criaLinha(usuario){
-    
-}
 
-function main(){
-    let maxVerstappen = fazGet("https://api.openf1.org/v1/drivers?last_name=Verstappen&session_key=latest")
-    max = JSON.parse(maxVerstappen)
-    console.log(max)
-}
-
-main()
+insertStandings();
