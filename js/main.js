4a901c882b79df42232302f7e589535f7dd53a4c
GuilhermeNSousa <nascimentoguilherme66@gmail.com>
2024-06-18 13:22:32 -0300

    Adição da primeira versão da classificação no index

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
+function mudaBgColor(pilotos, i, element){
+    switch(pilotos.MRData.StandingsTable.StandingsLists[0].DriverStandings[i].Constructors[0].name){
+        case 'Red Bull':
+            element.style.backgroundColor = "#025db3";
+            element.style.borderColor = "#025db3";
+            break;
+
+        case 'Ferrari':
+            element.style.backgroundColor = "#dd1111";
+            element.style.backgroundColor = "#dd1111";
+            break;
+
+        case 'McLaren':
+            element.style.backgroundColor = "#ff7b00";
+            element.style.borderColor = "#ff7b00";
+            break;
+
+        case 'Mercedes':
+            element.style.backgroundColor = "#08d2be";
+            element.style.borderColor = "#08d2be";
+            break;
+
+        case 'Aston Martin':
+            element.style.backgroundColor = "#007b6e";
+            element.style.borderColor = "#007b6e";
+            break;
+
+        case 'RB F1 Team':
+            element.style.backgroundColor = "#012a6d";
+            element.style.borderColor = "#012a6d";
+            break;
+
+        case 'Alpine F1 Team':
+            element.style.backgroundColor = "#da68e6";
+            element.style.borderColor = "#da68e6";
+            break;
+
+        case 'Sauber':
+            element.style.backgroundColor = "#68e670";
+            element.style.borderColor = "#68e670";
+            break;
+
+        case 'Williams':
+            element.style.backgroundColor = "#949794";
+            element.style.borderColor = "#949794";
+            break;
+
+        case 'Haas F1 Team':
+            element.style.backgroundColor = "#474747";
+            element.style.borderColor = "#474747";
+            break;
+
+        default:
+            element.style.backgroundColor = "red";
+            element.style.borderColor = "red";
+            break;
+    }
+}
+
 function insertStandings(){
     fetch("http://ergast.com/api/f1/2024/driverStandings.json")
         .then((response) => response.json())
@@ -7,7 +66,6 @@ function insertStandings(){
             var cards = document.querySelector("#cards");
 
             for (var i = 0; i < 3; i++){
-                console.log(standings[i]);
 
                 var card = document.createElement("div");
                 card.classList.add("card");
@@ -20,10 +78,12 @@ function insertStandings(){
                 picture.src = ".\\img\\Pilots\\" + standings[i].Driver.givenName + "_" + standings[i].Driver.familyName + ".png";
                 
                 var texts = document.createElement("div");
-                texts.classList.add("cardTexts")
+                texts.classList.add("cardTexts");              
+
+                mudaBgColor(pilotos, i, texts);
 
                 var pilotName = document.createElement("h3");
-                pilotName.classList.add("firstName")
+                pilotName.classList.add("firstName");
                 pilotName.textContent = standings[i].Driver.givenName;
 
                 var pilotLastName = document.createElement("h3");
@@ -33,7 +93,7 @@ function insertStandings(){
                 var position = document.createElement("h3");
                 position.textContent = "Position: " + standings[i].position;
 
-                pilotName.appendChild(pilotLastName)
+                pilotName.appendChild(pilotLastName);
                 texts.appendChild(pilotName);
 
                 elements.appendChild(picture);
@@ -48,5 +108,46 @@ function insertStandings(){
     })
 }
 
+function insertStandingsList(){
+    fetch("http://ergast.com/api/f1/2024/driverStandings.json")
+        .then((response) => response.json())
+        .then((pilotos) => {
+            var standings = pilotos.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+            var list = document.querySelector("#pList");
+            console.log(standings);
+
+            for(var i = 0; i < 10; i++){
+                var listElement = document.createElement("li");
+
+                var position = document.createElement("h3")
+                position.classList.add("position");
+                position.textContent = standings[i].position;
+
+                var liFN = document.createElement("h3");
+                liFN.classList.add("firstName");
+                liFN.textContent = standings[i].Driver.givenName;
+
+                var liLN = document.createElement("h3");
+                liLN.classList.add("lastName");
+                liLN.textContent = standings[i].Driver.familyName;
+                
+                var team = document.createElement("h3");
+                team.textContent = standings[i].Constructors[0].name;
+                team.classList.add("team");
+
+                var points = document.createElement("h3");
+                points.textContent = standings[i].points + " PTS";
+                points.classList.add("points");
+
+                listElement.appendChild(position);
+                listElement.appendChild(liFN);
+                listElement.appendChild(liLN);
+                listElement.appendChild(team);
+                listElement.appendChild(points);
+                
+                list.appendChild(listElement);
+            }
+        })};
 
 insertStandings();
+insertStandingsList();
